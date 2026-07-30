6dfa2d504149aec4defccfce8fc6c8234d352721
zeqi <a202128502@163.com>
2026-07-30 07:48:43 +0000

    完善对简体中文的支持
    Signed-off-by: zeqi <a202128502@163.com>

--- translations/deepin-picker_zh_CN.ts
+++ translations/deepin-picker_zh_CN.ts
@@ -20,25 +20,3 @@
     </message>
 </context>
 </TS>
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.1" language="zh_CN">
-<context>
-    <name>Clipboard</name>
-    <message>
-        <source>Copy color %1 to clipboard</source>
-        <translation>拷贝颜色%1到剪切板</translation>
-    </message>
-</context>
-<context>
-    <name>MainWindow</name>
-    <message>
-        <source>GXDE Picker is a quick and easy screen color picking tool. RGB and HEX codes are obtained on click and auto saved to the clipboard.</source>
-        <translation>GXDE 取色器是一款帮助您快速在屏幕上取色的工具。并且能根据您拾取的颜色得到RGB、HEX代码。打开软件后，鼠标经过的地方即为GXDE 取色器取色的区域。</translation>
-    </message>
-    <message>
-        <source>GXDE Picker</source>
-        <translation>GXDE 取色器</translation>
-    </message>
-</context>
-</TS>
